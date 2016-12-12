ed5d2a6e5d7bff2f77c966248b79239527f02528
maldeirra <malde.clement@hotmail.fr>
2016-12-12 20:00:00 +0100

    Add zombie who walk left to right with sound when walking

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+	public float min = 0f;
+	public float max = 10f;
+
+	private float speed = 0;
+	private AudioSource walk;
+
+	void Start()
+	{
+		walk = GetComponent<AudioSource> ();
+		walk.Play ();
+	}
+
+	void Update () 
+	{
+		speed = Random.Range (min, max);
+
+		Move ();
+	}
+
+	void Move()
+	{
+		transform.position += Vector3.left * speed * Time.deltaTime;
+	}
+}
