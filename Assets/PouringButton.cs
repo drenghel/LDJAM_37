cdda2873c24a267adde6602030a7da956b503f1c
Andre-Rb <andre.k.rabe@gmail.com>
2016-12-12 13:12:24 +0100

    Place click collider for MixerMachine

--- Assets/PouringButton.cs
+++ Assets/PouringButton.cs
@@ -1,14 +1,29 @@
 ﻿using UnityEngine;
 
-public class PouringButton : MonoBehaviour {
+public class PouringButton : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer _overSpriteRenderer;
+    [SerializeField] private Mixer _mixer;
+    private void Start()
+    {
+        _overSpriteRenderer.enabled = false;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void OnMouseUpAsButton()
+    {
+
+
+        Debug.Log("Click on pourring !" + _mixer.gameObject.transform.parent.name);
+    }
+
+    private void OnMouseOver()
+    {
+        _overSpriteRenderer.enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        _overSpriteRenderer.enabled = false;
+    }
 }
