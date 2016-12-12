2705531f2b3ba96e38b50b377183a03b53eb893b
Andre-Rb <andre.k.rabe@gmail.com>
2016-12-12 23:45:11 +0100

    Adding clickable stuff on closeup center station

---
+++ Assets/Microscope.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Microscope : MonoBehaviour {
+    [SerializeField]private SpriteRenderer _overSpriteRenderer;
+
+
+    //TODO should be a child class
+
+    private void OnMouseOver()
+    {
+        _overSpriteRenderer.enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        _overSpriteRenderer.enabled = false;
+    }
+
+
+    void Start()
+    {
+        _overSpriteRenderer.enabled = false;
+
+    }
+}
