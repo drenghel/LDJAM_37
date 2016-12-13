8432081f9dc57eab82db0b68e3d3b8d76d147cd9
maldeirra <malde.clement@hotmail.fr>
2016-12-13 01:33:06 +0100

    Try move camera without look At

--- Assets/Scripts/Joueur/Hide.cs
+++ Assets/Scripts/Joueur/Hide.cs
@@ -7,11 +7,32 @@ public class Hide : MonoBehaviour
 	public bool hide = false;
 	public Sprite hidden;
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.tag == "Table") 
-		{
+	private SpriteRenderer spriteRenderer;
 
+	void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+
+		Debug.Log ("PATATE");
+
+		if (other.gameObject.tag == "Table" && Input.GetKeyDown(KeyCode.LeftControl)) 
+		{
+			Debug.Log ("HIDE");
+			spriteRenderer.sprite = hidden;
+
+			if (!hide) 
+			{
+				hide = true;
+			} 
+			else 
+			{
+				hide = false;
+			}
 		}
 	}
+	
 }
